6810f8622d20d25df2e7e852e45779fd42911dda
Christian Boos <cboos@neuf.fr>
2010-07-08 21:03:45 +0200

    WikiDom: add the parser tests to the main wiki test suite

--- trac/wiki/tests/__init__.py
+++ trac/wiki/tests/__init__.py
@@ -3,7 +3,7 @@ import unittest
 
 import trac.wiki.api
 import trac.wiki.formatter
-from trac.wiki.tests import formatter, macros, model, wikisyntax
+from trac.wiki.tests import formatter, macros, model, parser, wikisyntax
 from trac.wiki.tests.functional import functionalSuite
 
 def suite():
@@ -12,6 +12,7 @@ def suite():
     suite.addTest(formatter.suite())
     suite.addTest(macros.suite())
     suite.addTest(model.suite())
+    suite.addTest(parser.suite())
     suite.addTest(wikisyntax.suite())
     suite.addTest(doctest.DocTestSuite(trac.wiki.api))
     suite.addTest(doctest.DocTestSuite(trac.wiki.formatter))
