ff8024e6d3209a43174ca812e34c306d5842c7cd
Christian Boos <cboos@bct-technology.com>
2016-03-18 20:16:09 +0100

    appveyor: still no luck with conda and svn

--- contrib/appveyor.ps1
+++ contrib/appveyor.ps1
@@ -210,7 +210,9 @@ function Trac-Install {
     if ($pyIsConda) {
         & conda.exe install -qy $condaCommonPackages
         & conda.exe list
+        & python.exe -m conda list
         & python.exe -c "from svn import core; print core.SVN_VER_NUM"
+        Write-Host "PATH is", $env:Path
     }
 
     if ($usingMysql) {
